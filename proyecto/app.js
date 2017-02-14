45c19e18524b05faf8eae89f5fc6885f46ce3879
Anhy <anahi.z.a@gmail.com>
2017-02-14 10:30:59 -0600

    Leer Parametros

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
-app.use('/estatico',express.static('public'));
-
+app.use('/public',express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'pug')
 
 app.get("/", function (req, res) {
@@ -11,6 +13,13 @@ app.get("/", function (req, res) {
 
 app.get("/login", function (req, res) {
   res.render("login")
+})
+
+app.post("/users", function (req, res) {
+  console.log(req.body.email);
+  console.log(req.body.password);
+  res.send("Recibimos los datos")
+  
 })
 
 
